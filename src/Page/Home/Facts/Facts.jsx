d21c69441d889dce92aeeb69c9d2e3b51e598c29
ripon4521 <riponakondo4521@gmail.com>
2023-12-21 21:06:25 +0600

    banner2 section

---
+++ src/Page/Home/Facts/Facts.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const DataSection = () => {
+  return (
+    <section>
+      {/* Container */}
+      <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+        {/* Title */}
+        <h2 className="text-center text-3xl font-bold md:text-5xl">Backed up by real data</h2>
+        <p className="mx-auto mb-8 mt-4 max-w-lg items-center text-center text-sm text-[#636262] sm:text-base md:mb-12 lg:mb-16">Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam,purus sit amet luctus magna fringilla urna</p>
+        {/* Content */}
+        <div className="mx-auto flex w-full max-w-4xl flex-col flex-wrap justify-between gap-5 px-16 py-8 sm:flex-row md:gap-6">
+          <DataItem title="Blocks" value="200+" />
+          <DataItem title="Templates" value="45" />
+          <DataItem title="Customers" value="1500+" />
+          <DataItem title="Support Tickets" value="890" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const DataItem = ({ title, value }) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-sm">{title}</p>
+      <h2 className="text-3xl font-bold md:text-6xl">{value}</h2>
+    </div>
+  );
+};
+
+export default DataSection;
